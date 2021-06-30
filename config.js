73a07755dbb7568eac9832db0ba3064e64f55345
phucvu8825 <86733103+phucvu8825@users.noreply.github.com>
2021-07-01 01:29:35 +1000

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
+    introTitle: 'sao m có thể hãm như thế duoc nhỉ yeu êm!',
     introDesc: `Trái đất vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
